11f1848ee8e69fd848e5fc20e96d814c4a161ead
AnshVM <anshmalik2002@gmail.com>
2021-09-07 01:51:44 +0530

    Setup user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const Router = require('express').Router();
+const userController = require('../controllers/userController');
+const auth = require('../lib/auth');
+
+Router.post('/signup',userController.signup);
+Router.get('/login',userController.login);
+Router.get('/logout',auth,userController.logout);
+Router.get('/:id',auth,userController.getUserById);
+Router.get('/',auth,userController.getCurentUser);
+
+module.exports = Router;
