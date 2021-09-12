03b54585e2d91629595c29e46f8e5fb20cc68deb
AnshVM <anshmalik2002@gmail.com>
2021-09-12 16:01:53 +0530

    Fixed /profile/undefined

--- client/src/components/topbar.jsx
+++ client/src/components/topbar.jsx
@@ -27,7 +27,7 @@ function Right() {
         return (
             <div className="flex flex-row items-center">
                 <Button onClick={handleLogout} className="mr-3" colorScheme="teal" variant="ghost">Logout</Button>
-                <Link to={"/profile/"+user.id}>
+                <Link to={"/profile/"+user._id}>
                     <AccountCircleIcon />
                 </Link>
                 <Text color="teal"><p className="font-semibold">{user.username}</p></Text>
